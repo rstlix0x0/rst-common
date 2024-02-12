08567ab636b6d6e3a1be281fbe913d5831cccf17
rstlix0x0 <rstlix.dev@gmail.com>
2024-02-12 07:39:37 +0700

    chore: adding example for thiserror

--- examples/with-errors/main.rs
+++ examples/with-errors/main.rs
@@ -1,4 +1,12 @@
+use std::result::Result as CoreResult;
 use rst_common::with_errors::anyhow::{Result, Ok, bail};
+use rst_common::with_errors::thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("something went wrong: `{0}`")]
+    Msg(String) 
+}
 
 fn is_okay() -> Result<String> {
     Ok("hello world".to_string())
@@ -9,10 +17,17 @@ fn is_error() -> Result<()> {
     Ok(())
 }
 
+fn from_thiserror() -> CoreResult<(), MyError> {
+    Err(MyError::Msg("testing".to_string()))
+} 
+
 fn main() {
     let okay = is_okay();
     println!("{}", okay.unwrap());
 
     let err = is_error();
-    println!("{}", err.unwrap_err())
+    println!("{}", err.unwrap_err());
+
+    let thiserr_err = from_thiserror();
+    println!("{}", thiserr_err.unwrap_err())
 }
